5ef8d37d8f38b383a93b4f915276267d0cb4983b
Matheus Pegorari <pegorari42@gmail.com>
2024-04-16 00:48:39 -0300

    ngFor ngIf ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input',
+      autoria: 'Componente Filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Lorem ipsum dolor sit aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaamet consectetur adipisicing elit. Quasi facilis perspiciatis harum cupiditate? Sint maiores voluptas autem repellendus modi ducimus, quaerat vel eaque ut eius vero et sunt quibusdam voluptates.',
+      autoria: 'Componente Filho',
+      modelo: 'modelo2',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
